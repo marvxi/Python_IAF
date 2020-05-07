f8f35d4040ea35032bedba6b6e0d6335c0f30db0
MKrawutschke <m.krawutschke@tu-bs.de>
2020-05-07 21:12:39 +0200

    UE1

---
+++ UE_1/A_2_3.py
@@ -0,0 +1,17 @@
+x = []
+for i in range(11):
+    x.append(i)
+
+
+for i in range(1,len(x)):
+    print('| x%d ' %(x[i]),end='')
+print('|',end='')
+print('\n')
+print('-----------------------------------------------------',end='')
+print('\n')
+
+for i in range(1,len(x)):
+    print('|  %d ' %(x[i]),end='')
+print('|', end='')
+
+
