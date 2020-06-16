fba85f61509614c6ad437183c390cde41d377108
MKrawutschke <m.krawutschke@tu-bs.de>
2020-06-16 21:50:36 +0200

    UE2 beendet

--- TEST.py
+++ TEST.py
@@ -1,16 +1,14 @@
-import openpyxl as opx
+def encryption(text):
+    text = text.upper() #Macht alle Buchstaben groß!
+    encrypted_message = ''
+    for character in text:
+        num = ord(character) # Unicode, jedes Zeichen hat eine bestimmte Zahl int wert
+        new_num = num + 3    # verändere den Unicode also + 3
+        if new_num > ord('Z'):  # Wenn new_num größer als 90 ist:
+            new_num = new_num - 26  # new_num wird um 26 verringert
+            new_character = chr(new_num)
+            encrypted_message = encrypted_message + new_character
+    # Ausgabe
+    return encrypted_message
 
-fileName = r'C:\Users\Marvi\OneDrive\03_Studium Unterlagen\10_Master\03_Semester\02_Python\02_UE/MeineDaten.xlsx'
-wb = opx.load_workbook(filename=fileName)
-
-ws = wb['Tabelle1']
-
-# Wie lang ist das Datenset?
-
-count = 0
-field = 'A'
-data = ''
-
-while data != None:
-    count+=1
-    data = ws['A' + str(count)].value
+print(encryption('halloy'))
--- UE_2/A_16.py
+++ UE_2/A_16.py
@@ -1,35 +1,34 @@
 text = open(r'C:\Users\Marvi\Documents\Pycharm_Projects\Python_IAF\UE_2\Text.txt')
 text = str(text.readlines())
 
-
 def encryption(text):
     text = text.upper() #Macht alle Buchstaben groß!
     encrypted_message = ''
     for character in text:
+        #print(character)
         num = ord(character) # Unicode, jedes Zeichen hat eine bestimmte Zahl int wert
         new_num = num + 3    # verändere den Unicode also + 3
+        #print(chr(new_num))
         if new_num > ord('Z'):  # Wenn new_num größer als 90 ist:
             new_num = new_num - 26  # new_num wird um 26 verringert
-            new_character = chr(new_num)
-            encrypted_message = encrypted_message + new_character
+        new_character = chr(new_num)
+        encrypted_message = encrypted_message + new_character
     # Ausgabe
     return encrypted_message
 
 encrp = encryption(text)
+print(encrp)
 
 def decryption(text):
     decrpted_message = ''
     for character in text:
         num = ord(character)
-        new_num = num + 26
+        new_num = num - 3
         if new_num > ord('Z'):
-            new_num = new_num - 3
-            new_character = chr(new_num)
-
-            decrpted_message = decrpted_message + new_character
+            new_num = new_num + 26
+        new_character = chr(new_num)
+        decrpted_message = decrpted_message + new_character
 
     return decrpted_message
 
-print(decryption(encrp))
-
-
+print(decryption('SBWKRQ#PDFKW#VSDVV$'))
